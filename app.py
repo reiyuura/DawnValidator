1523e79a9ef0c035b60565dd4efef1c38586e93c
Reiyuura <yuiireichi@gmail.com>
2024-10-02 11:52:08 +0700

    fix some error and add function keep alive

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import asyncio
 from dotenv import load_dotenv
 from logger import TelegramLogger, setup_logger
 from colorama import Fore, Style, init
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Initialize colorama
 init(autoreset=True)
@@ -30,13 +30,13 @@ def prompt_for_env_variables():
     # Prompt for TOKEN
     token = os.getenv("TOKEN")
     if not token:
-        token = input("Please enter your Dawn Token: ")
+        token = input("Please enter your Token: ")
         inputs.append(f"TOKEN={token}")
 
     # Prompt for TELEGRAM_TOKEN
     telegram_token = os.getenv("TELEGRAM_TOKEN")
     if not telegram_token:
-        telegram_token = input("Please enter your Telegram Bot Token: ")
+        telegram_token = input("Please enter your Telegram Token: ")
         inputs.append(f"TELEGRAM_TOKEN={telegram_token}")
 
     # Prompt for CHAT_ID
@@ -62,6 +62,7 @@ setup_logger(telegram_logger)
 # Request URLs
 POINTS_URL = "https://www.aeropres.in/api/atom/v1/userreferral/getpoint"
 PING_URL = "https://www.aeropres.in/dawnserver/ping"
+KEEPALIVE_URL = "https://www.aeropres.in/chromeapi/dawn/v1/userreward/keepalive"
 
 # Function to set up headers
 def create_headers(token):
@@ -71,12 +72,16 @@ def create_headers(token):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
     }
 
+email = ""
+
 # Function to fetch points
 async def fetch_points(headers):
+    global email
     try:
         response = requests.get(POINTS_URL, headers=headers, verify=False)
         response.raise_for_status()
         data = response.json().get("data", {}).get("rewardPoint", {})
+        email = data.get('userId','')
 
         total_points = sum([
             data.get('points', 0),
@@ -87,29 +92,22 @@ async def fetch_points(headers):
 
         log_message = (
             f"{'ID'.ljust(15)}: {data.get('_id')}\n"
-            f"{'User'.ljust(15)}: {data.get('userId')}\n"
+            f"{'User'.ljust(15)}: {email}\n"
             f"{'Total Points'.ljust(15)}: {total_points}\n"
         )
 
         # Log to console with color and timestamp
-        logging.info(f"\n{Fore.GREEN}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Fetching points...\n{Fore.YELLOW}{log_message}")
+        logging.info(f"{Fore.GREEN}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Fetching points...\n{Fore.YELLOW}{log_message}")
 
-        # Send log message to Telegram (dengan pengecekan jika terjadi error)
-        try:
-            await telegram_logger.send_message("Fetching points: \n" + log_message)
-        except Exception as e:
-            logging.error(f"{Fore.RED}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Failed to send log to Telegram: {e}")
-    
+        # Send log message to Telegram
+        await telegram_logger.send_message(log_message)
+
         return total_points  # Return total points for later use
 
     except requests.exceptions.RequestException as e:
-        logging.error(f"\n{Fore.RED}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Error fetching points: {e}")
-        # Send error message to Telegram (dengan pengecekan jika terjadi error)
-        try:
-            await telegram_logger.send_message(f"Error fetching points: {e}")
-        except Exception as e:
-            logging.error(f"{Fore.RED}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Failed to send error log to Telegram: {e}")
-
+        logging.error(f"{Fore.RED}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Error fetching points: {e}")
+        # Send error message to Telegram
+        await telegram_logger.send_message(f"Error fetching points: {e}")
         return 0  # Return 0 points on error
 
 # Function to ping the server
@@ -117,35 +115,49 @@ async def ping_server(headers):
     try:
         response = requests.get(PING_URL, headers=headers, verify=False)
         response.raise_for_status()
-        logging.info(f"\n{Fore.GREEN}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Ping successful. \nServer response: \n{response.text}")
-        # Send success message to Telegram (dengan pengecekan jika terjadi error)
-        try:
-            await telegram_logger.send_message(f"Ping successful. Server response: {response.text}")
-        except Exception as e:
-            logging.error(f"{Fore.RED}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Failed to send ping log to Telegram: {e}")
+        logging.info(f"{Fore.GREEN}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Ping successful. Server response: {response.text}")
+        # Send success message to Telegram
+        await telegram_logger.send_message(f"Ping successful. Server response: {response.text}")
     except requests.exceptions.RequestException as e:
-        logging.error(f"\n{Fore.RED}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Ping failed: {e}")
-        # Send error message to Telegram (dengan pengecekan jika terjadi error)
-        try:
-            await telegram_logger.send_message(f"Ping failed: {e}")
-        except Exception as e:
-            logging.error(f"{Fore.RED}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Failed to send error log to Telegram: {e}")
+        logging.error(f"{Fore.RED}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Ping failed: {e}")
+        # Send error message to Telegram
+        await telegram_logger.send_message(f"Ping failed: {e}")
+
+# Function to send keepalive request
+async def keep_alive(headers, last_keepalive_log_time):
+    try:
+        response = requests.post(KEEPALIVE_URL, headers=headers, json={
+            "username": email,
+            "extensionid": "fpdkjdnhkakefebpekbdhillbhonfjjp",
+            "numberoftabs": 0,
+            "_v": "1.0.9"
+        }, verify=False, timeout=30)
+
+        if response.status_code == 200:
+            logging.info(f"{Fore.GREEN}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Keepalive successful.")
+            current_time = datetime.now()
+            if current_time - last_keepalive_log_time >= timedelta(minutes=5):
+                await telegram_logger.send_message("Keepalive successful.")
+                last_keepalive_log_time = current_time
+        else:
+            logging.warning(f"{Fore.YELLOW}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Keepalive returned status code {response.status_code}")
+            await telegram_logger.send_message(f"Keepalive returned status code {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"{Fore.RED}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Keepalive failed: {e}")
+        await telegram_logger.send_message(f"Keepalive failed: {e}")
 
 # Main execution flow
 async def main():
     logging.basicConfig(level=logging.INFO)  # Set logging level to INFO
     headers = create_headers(TOKEN)
 
+    # Inisialisasi waktu log terakhir
+    last_keepalive_log_time = datetime.now() - timedelta(minutes=5)
     while True:
         await ping_server(headers)
+        await keep_alive(headers, last_keepalive_log_time)  # Keep the connection alive with the new URL
         await fetch_points(headers)
-        await asyncio.sleep(300)  # Delay before the next loop iteration
+        await asyncio.sleep(500)  # Delay before the next loop iteration
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except Exception as e:
-        logging.error(f"{Fore.RED}{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Critical error in main: {e}")
-
-
-### Thanks to @rainlovy99 for methode
+    asyncio.run(main())
